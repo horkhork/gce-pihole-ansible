88db99e7469abb5aa8b4cf0b89b0ec3c7b2bb5f2
Steve Sosik <ssosik@akamai.com>
2019-01-02 20:39:35 -0500

    Add initial bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt install git make
+#sudo apt install git make python-setuptools gcc python-dev libffi-dev libssl-dev python-packaging
+
+git clone git://github.com/ansible/ansible.git
+cd ansible
+git checkout stable-2.6
+make
+sudo make install
+
+ansible --version
